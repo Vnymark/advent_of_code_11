a709937ed3efda69dea07e8b28d431cca8f5c94b
Viktor Nymark <vnymark@gmail.com>
2019-04-03 20:48:28 +0200

    Add project files.

---
+++ advent_of_code_11/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace advent_of_code_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
