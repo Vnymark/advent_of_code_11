b31ad038c51e565d0aa759b8cd907971b161fe5d
Viktor Nymark <vnymark@gmail.com>
2019-04-04 21:17:45 +0200

    Added more notes and created functionality to create the fuelcells and add them to a list.

---
+++ advent_of_code_11/FuelCell.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace advent_of_code_11
+{
+    class FuelCell
+    {
+        public List<int> Cordinates { get; set; }
+        public int RackId { get; set; }
+        public int PowerLevel { get; set; }
+    }
+}
--- advent_of_code_11/Program.cs
+++ advent_of_code_11/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace advent_of_code_11
 {
@@ -6,7 +7,35 @@ namespace advent_of_code_11
     {
         static void Main(string[] args)
         {
-            int input = 1955;
+            //Grid serial number is the puzzle input.
+            int gridSerialNumber = 1955;
+            List<FuelCell> FuelCellList = new List<FuelCell>();
+
+            AddFuelCells();
+            Console.WriteLine();
+
+            void AddFuelCells()
+            {
+                int x = 1;
+                while (x <= 300)
+                {
+                    int y = 1;
+                    while (y <= 300)
+                    {
+                        FuelCell fuelCell = new FuelCell
+                        {
+                            Cordinates = new List<int>
+                        {
+                            x,
+                            y
+                        }
+                        };
+                        FuelCellList.Add(fuelCell);
+                        y++;
+                    }
+                    x++;
+                }
+            }
         }
     }
 }
